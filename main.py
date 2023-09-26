ebc6e462839bbb9a7f99a32a498ae5bb7953fd60
MaddoSaru <riquelme.madmonkey@gmail.com>
2023-09-26 14:23:40 +0800

    fix(blockchain_to_db/main.py): change to black format

--- main.py
+++ main.py
@@ -4,30 +4,28 @@ import fastapi
 app = fastapi.FastAPI()
 blockchain = blockchain.blockchain()
 
+
 @app.get("/mine_block/")
-def mine_block(
-    data : str
-):
+def mine_block(data: str):
     if not blockchain.chain_validation():
         return fastapi.HTTPException(
-            status_code = 400,
-            detail = "The blockchain is invalid"
+            status_code=400, detail="The blockchain is invalid"
         )
-    block = blockchain.mine_block(data = data)
-    
+    block = blockchain.mine_block(data=data)
+
     return block
+
 
 @app.get("/get_blockchain/")
 def get_blockchain():
     if not blockchain.chain_validation():
         return fastapi.HTTPException(
-            status_code = 400,
-            detail = "The blockchain is invalid"
+            status_code=400, detail="The blockchain is invalid"
         )
-    
+
     return blockchain.chain
+
 
 @app.get("/validate_blockchain/")
 def validate_blockchain():
-
     return blockchain.chain_validation()
